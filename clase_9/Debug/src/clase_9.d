8a45a43c4fbd0e75acff0ee28d87e5ac29b491bd
alumno <alumno@lab6pc00>
2019-09-18 18:50:54 -0300

    comienzo de la clase

---
+++ clase_9/Debug/src/clase_9.d
@@ -0,0 +1,1 @@
+src/clase_9.o: ../src/clase_9.c
---
+++ clase_9/src/clase_9.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : clase_9.c
+ Author      : agn
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	return 0;
+}
