8f4929be7f9d0bfded0420d520231d8d86175f66
alumno <alumno@lab6pc00>
2019-09-18 21:29:50 -0300

    cambio de maquina

---
+++ Prog_Clase9/Estructuras/Estructuras1-Definicion/main.c
@@ -0,0 +1,20 @@
+/*Estructuras - Definicion y Declaracion
+*/
+#include <stdio.h>
+#include <stdlib.h>
+//Definición de Estructura
+//struct datosPersonales
+typedef struct {
+    char nombre[20];
+    char apellido[20];
+    char calle[20];
+    int numero;
+}datosPersonales;
+//};
+int main()
+{   //Declaracion de variable agenda de tipo datosPersonales
+    datosPersonales agenda;
+//struct datosPersoanles agenda;
+
+    return 0;
+}
--- clase_9/src/clase_9.c
+++ clase_9/src/clase_9.c
@@ -22,22 +22,15 @@ typedef struct
 
 	}datosPersonales;
 
-int funcMenu(int *opcion);
+int funcMenu(int opcion);
+int ingresoEstructuras();
+
+
 
 int main(void) {
 
-	datosPersonales vecAgenda[3];
-	// --- LATER ADD ", vecContactos[3], vecAmigos[3]" --- //
-	int i;
 
-	for(i=0;i<3;i++)
-	{
-		printf("\nIngrese nombre: ");
-		gets(vecAgenda[i].nombre);
-		printf("\nIngrese apellido: ");
-		gets(vecAgenda[i].apellido);
-
-	}
+	ingresoEstructuras();
 
 
 	//vecAgenda[0].nombre
@@ -45,45 +38,87 @@ int main(void) {
 	// --- strcmp (sTRINGcOMPARE) - compara characters mayor/menor --- //
 	// ----- 1. ENLISTAR - 2.ALTA - 3.BAJA - 4. MODI - 5.ORDENAR ----- //
 
-	return 0;
 }
 
-int funcMenu(int *opcion)
+int funcMenu(int opcion)
 {
 	int valor;
-	valor = *opcion;
 	int retorno;
 	retorno = -1;
 
-	switch (valor)
-	​{
-		case 1:
-	    	// ENLISTAR
-	    	break;
+	printf("\n--- MENU ---\nLISTA DE OPCIONES");
+	printf("\n1: Enlistar\n2: Alta\n3: Baja");
+	printf("\n4: Modificar\n5: Ordenar\n");
+	printf("\nIngrese una opcion: ");
+	scanf(&valor);
 
-	    case '2':
-			// ALTA
-	    	break;
+	if(valor == 1)
+	{
+		// LISTA
+		retorno = 0;
+	}
 
-	    case '3':
-			// BAJA
-	    	break;
+	if(valor == 2)
+	{
+		// ALTA
+		retorno = 0;
+	}
 
-	    case '4':
-			// MODI
-	    	break;
+	if(valor == 3)
+	{
+		// BAJA
+		retorno = 0;
+	}
 
-	    case '5':
-			// ORDENAR
-	    	break;
+	if(valor == 4)
+	{
+		// MODI
+		retorno = 0;
+	}
 
-	    default:
-	    	// RETORNO ERROR
-	    	break;
+	if(valor == 5)
+	{
+		// ORDER
+		retorno = 0;
+	}
+
+	printf("El valor ingresado fue: %d",valor);
+	return retorno;
+}
+
+int ingresoEstructuras()
+{
+	int i;
+	int retorno;
+	retorno = -1;
+
+	datosPersonales vecAgenda[3];
+	// --- LATER ADD ", vecContactos[3], vecAmigos[3]" --- //
+
+	for(i=0;i<3;i++)
+	{
+		printf("\nIngrese nombre: ");
+		gets(vecAgenda[i].nombre);
+		//strcpy(vecAgenda[i].nombre,);
+		__fpurge(stdin);
+
+		printf("\nIngrese apellido: ");
+		gets(vecAgenda[i].apellido);
+		__fpurge(stdin);
+		printf("\nIngrese direccion: ");
+		gets(vecAgenda[i].calle);
+		__fpurge(stdin);
+		printf("\nIngrese numeracion de la calle: ");
+		gets(vecAgenda[i].numero);
+		__fpurge(stdin);
+
+		printf("\nTESTNOM%s", vecAgenda[i].nombre);
+	    printf("\nTESTAPEL%s", vecAgenda[i].apellido);
+	    printf("\nTESTCALL%s", vecAgenda[i].calle);
+	    printf("\nTESTNUME%d", vecAgenda[i].numero);
+
 	}
 
 	return retorno;
 }
-
-
 
